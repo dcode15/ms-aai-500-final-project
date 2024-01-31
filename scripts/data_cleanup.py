bfd94c57e7dd7bdd7e5022a86f9847a28b5a9cb6
Douglas Code <dcode10@gmail.com>
2024-01-30 20:40:04 -0500

    Added output to CSV with cleaned data format.

--- scripts/data_cleanup.py
+++ scripts/data_cleanup.py
@@ -1,6 +1,7 @@
 from typing import TypedDict, List
 import os
 import glob
+from pathlib import Path
 
 import pandas as pd
 
@@ -57,13 +58,13 @@ tables: List[TableSpec] = [
      "end_row": 25, "header_row": 7},
     {"file_name": "CDD_Monthly_Smoothed", "sheet_name": "CDD Monthly", "period": "MONTH", "start_row": 28,
      "end_row": 42, "header_row": 7},
-    {"file_name": "CDD_Seasonal_Unsmoothed", "sheet_name": "CDD Monthly", "period": "SEASON", "start_row": 11,
+    {"file_name": "CDD_Seasonal_Unsmoothed", "sheet_name": "CDD Seasonal", "period": "SEASON", "start_row": 11,
      "end_row": 25, "header_row": 7},
-    {"file_name": "CDD_Seasonal_Smoothed", "sheet_name": "CDD Monthly", "period": "SEASON", "start_row": 28,
+    {"file_name": "CDD_Seasonal_Smoothed", "sheet_name": "CDD Seasonal", "period": "SEASON", "start_row": 28,
      "end_row": 42, "header_row": 7},
-    {"file_name": "CDD_Monthly_Unsmoothed_Unstandardized", "sheet_name": "CDD Monthly", "period": "MONTH",
+    {"file_name": "CDD_Monthly_Unsmoothed_Unstandardized", "sheet_name": "CDD Unstandardized", "period": "MONTH",
      "start_row": 11, "end_row": 25, "header_row": 7},
-    {"file_name": "CDD_Monthly_Smoothed_Unstandardized", "sheet_name": "CDD Monthly", "period": "MONTH",
+    {"file_name": "CDD_Seasonal_Unsmoothed_Unstandardized", "sheet_name": "CDD Unstandardized", "period": "SEASON",
      "start_row": 32, "end_row": 46, "header_row": 28},
     {"file_name": "Rx5Day_Monthly_Unsmoothed", "sheet_name": "Rx5Day Monthly", "period": "MONTH", "start_row": 11,
      "end_row": 25, "header_row": 7},
@@ -75,8 +76,8 @@ tables: List[TableSpec] = [
      "end_row": 42, "header_row": 7},
     {"file_name": "Rx5Day_Monthly_Unsmoothed_Unstandardized", "sheet_name": "Rx5day Unstandardized", "period": "MONTH",
      "start_row": 11, "end_row": 25, "header_row": 7},
-    {"file_name": "Rx5Day_Monthly_Smoothed_Unstandardized", "sheet_name": "Rx5day Unstandardized", "period": "MONTH",
-     "start_row": 32, "end_row": 46, "header_row": 28},
+    {"file_name": "Rx5Day_Seasonal_Unsmoothed_Unstandardized", "sheet_name": "Rx5day Unstandardized",
+     "period": "SEASON", "start_row": 32, "end_row": 46, "header_row": 28},
     {"file_name": "T10_Monthly_Unsmoothed", "sheet_name": "T10 Monthly", "period": "MONTH", "start_row": 11,
      "end_row": 25, "header_row": 7},
     {"file_name": "T10_Monthly_Smoothed", "sheet_name": "T10 Monthly", "period": "MONTH", "start_row": 28,
@@ -87,7 +88,7 @@ tables: List[TableSpec] = [
      "end_row": 42, "header_row": 7},
     {"file_name": "T10_Monthly_Unsmoothed_Unstandardized", "sheet_name": "T10 Unstandardized", "period": "MONTH",
      "start_row": 11, "end_row": 25, "header_row": 7},
-    {"file_name": "T10_Monthly_Smoothed_Unstandardized", "sheet_name": "T10 Unstandardized", "period": "MONTH",
+    {"file_name": "T10_Seasonal_Unsmoothed_Unstandardized", "sheet_name": "T10 Unstandardized", "period": "SEASON",
      "start_row": 32, "end_row": 46, "header_row": 28},
     {"file_name": "T90_Monthly_Unsmoothed", "sheet_name": "T90 Monthly", "period": "MONTH", "start_row": 11,
      "end_row": 25, "header_row": 7},
@@ -99,7 +100,7 @@ tables: List[TableSpec] = [
      "end_row": 42, "header_row": 7},
     {"file_name": "T90_Monthly_Unsmoothed_Unstandardized", "sheet_name": "T90 Unstandardized", "period": "MONTH",
      "start_row": 11, "end_row": 25, "header_row": 7},
-    {"file_name": "T90_Monthly_Smoothed_Unstandardized", "sheet_name": "T90 Unstandardized", "period": "MONTH",
+    {"file_name": "T90_Seasonal_Unsmoothed_Unstandardized", "sheet_name": "T90 Unstandardized", "period": "SEASON",
      "start_row": 32, "end_row": 46, "header_row": 28},
     {"file_name": "WP90_Monthly_Unsmoothed", "sheet_name": "WP90 Monthly", "period": "MONTH", "start_row": 11,
      "end_row": 25, "header_row": 7},
@@ -111,18 +112,44 @@ tables: List[TableSpec] = [
      "end_row": 42, "header_row": 7},
     {"file_name": "WP90_Monthly_Unsmoothed_Unstandardized", "sheet_name": "WP90 Unstandardized", "period": "MONTH",
      "start_row": 11, "end_row": 25, "header_row": 7},
-    {"file_name": "WP90_Monthly_Smoothed_Unstandardized", "sheet_name": "WP90 Unstandardized", "period": "MONTH",
+    {"file_name": "WP90_Seasonal_Unsmoothed_Unstandardized", "sheet_name": "WP90 Unstandardized", "period": "SEASON",
      "start_row": 32, "end_row": 46, "header_row": 28},
-    {"file_name": "ACI_Combined_Monthly_Unsmoothed", "sheet_name": "ACI Combined Monthly", "period": "MONTH", "start_row": 11,
+    {"file_name": "ACI_Combined_Monthly_Unsmoothed", "sheet_name": "ACI Combined Monthly", "period": "MONTH",
+     "start_row": 11,
      "end_row": 25, "header_row": 7},
-    {"file_name": "ACI_Combined_Monthly_Smoothed", "sheet_name": "ACI Combined Monthly", "period": "MONTH", "start_row": 28,
+    {"file_name": "ACI_Combined_Monthly_Smoothed", "sheet_name": "ACI Combined Monthly", "period": "MONTH",
+     "start_row": 28,
      "end_row": 42, "header_row": 7},
-    {"file_name": "ACI_Combined_Seasonal_Unsmoothed", "sheet_name": "ACI Combined Seasonal", "period": "SEASON", "start_row": 11,
+    {"file_name": "ACI_Combined_Seasonal_Unsmoothed", "sheet_name": "ACI Combined Seasonal", "period": "SEASON",
+     "start_row": 11,
      "end_row": 25, "header_row": 7},
-    {"file_name": "ACI_Combined_Seasonal_Smoothed", "sheet_name": "ACI Combined Seasonal", "period": "SEASON", "start_row": 28,
+    {"file_name": "ACI_Combined_Seasonal_Smoothed", "sheet_name": "ACI Combined Seasonal", "period": "SEASON",
+     "start_row": 28,
      "end_row": 42, "header_row": 7}
 ]
 
 for table in tables:
     load_spreadsheet_data(table["sheet_name"], table["period"], table["start_row"], table["end_row"],
                           table["header_row"]).to_csv(f"../data/{table["file_name"]}.csv", index=False, header=False)
+
+
+script_path = os.path.abspath(__file__)
+parent_directory = os.path.dirname(os.path.dirname(script_path))
+data_folder_path = os.path.join(parent_directory, 'data')
+pattern = os.path.join(data_folder_path, '*Monthly*.csv')
+matching_files = glob.glob(pattern)
+
+running_df = None
+for file in matching_files:
+    df = pd.read_csv(file, header=[0, 1], index_col=0)
+    df_melted = df.melt(var_name=["Year", "Month"], value_name="Value", ignore_index=False)
+    df_melted.reset_index(inplace=True)
+    df_melted.columns.values[0] = "Region"
+    df_melted.columns.values[3] = Path(file).stem
+
+    if running_df is None:
+        running_df = df_melted
+    else:
+        running_df = pd.merge(running_df, df_melted, on=["Region", "Year", "Month"], how="outer")
+
+running_df.to_csv(f"../data/Cleaned_Monthly_Data.csv", index=False, header=True)
